187e2dab79a5252df69e5eb309ffa4b848dedb5c
JJY <jjySA009@qq.com>
2026-05-17 19:26:59 +0800

    完成数据集下载

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Traffic-Sign-Classification-Based-on-CNN
 【基于CNN的交通标志分类】
+
+文件目录结构：
+
+data：存放从 Kaggle 下载的 mini-traffic-detection-dataset 数据集。
+
+code：放置 PyTorch 代码文件，包括数据处理、模型定义、训练和预测的代码。
+
+results：用于保存训练过程中的关键数据（如损失、准确率等），以及预测结果。
+
+docs：可放入 README 文件，以及关于模型网络介绍等的文档。
+
+models：保存训练好的模型文件。
---
+++ code/data.py
@@ -0,0 +1,24 @@
+#本文件用于处理数据
+
+import kagglehub
+import shutil #shutil 是 Python 自带的标准库，无需额外安装，主要用来操作文件 / 文件夹。
+import os
+
+#step1:数据下载
+
+# Download latest version
+path=kagglehub.dataset_download("zoltanszekely/mini-traffic-detection-dataset")
+print("Path to dataset files:", path)
+
+#我不想它把数据下到C盘。但是kagglehub默认不支持直接指定路径。遂用移动文件夹的方式实现。
+
+# 自定义目标保存路径
+target_path = "D:\GitHub\Traffic-Sign-Classification-Based-on-CNN\data"
+
+# 遍历下载的文件，移动到指定路径
+for item in os.listdir(path):
+    src = os.path.join(path, item)    # 源文件路径
+    dst = os.path.join(target_path, item) # 目标路径
+    shutil.move(src, dst)
+
+print("已移动到指定路径:", target_path)
